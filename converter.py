94f1234d5b9b47bac9563808b50c53011d480804
Jorge <jorgejapao00@gmail.com>
2024-04-16 21:47:08 -0300

    Now it downloads the video

--- converter.py
+++ converter.py
@@ -3,17 +3,12 @@ from pytube import YouTube
 
 
 def download(video):
-    yt = YouTube(video).streams.last().download()
+    yt = YouTube(video)
     title=yt.title
-    yt.streams
-    filter(progressive=True, file_extension='mp4')
-    yt.order_by('resolution')
-    yt.desc()
-    yt.last()
     a=input(f'Download {title}? (Y/N) ')
     if a == 'Y' or a == 'y':
         print('Downloading...')
-        yt.download()
+        yt.streams.filter(progressive=False, file_extension='mp4').order_by('resolution').last().download()
     else:
         return
 
