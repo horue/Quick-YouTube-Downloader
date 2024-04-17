9f25c5f911ce22bdb128e62d0b0ea4f09f635306
Jorge <jorgejapao00@gmail.com>
2024-04-17 18:56:48 -0300

    Audio download added

--- converter.py
+++ converter.py
@@ -6,13 +6,17 @@ def download(video):
     yt = YouTube(video)
     title=yt.title
     streams = yt.streams.filter(progressive=True)
+    audios = yt.streams.filter(only_audio=True)
 
 
     print('Download options: ')
     for i, stream in enumerate(streams):
         print(f'{i+1} - {stream}')
+    for i, audio in enumerate(audios):
+        print(f'{i+len(streams)+1} - {audio}')
 
-    a=input(f'Download {title}? (Y/N) ')
+
+    a=input(f'What option do you want to download {title}? ')
     if a == 'Y' or a == 'y':
         print('Downloading...')
         yt.download()
