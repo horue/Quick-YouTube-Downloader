6d4ddf4a1969407f776d253c5c142414951a4c16
Jorge <jorgejapao00@gmail.com>
2024-04-16 21:33:26 -0300

    Its working, but downloading mp3 for some reason

--- converter.py
+++ converter.py
@@ -3,8 +3,13 @@ from pytube import YouTube
 
 
 def download(video):
-    yt = YouTube(video)
+    yt = YouTube(video).streams.last().download()
     title=yt.title
+    yt.streams
+    filter(progressive=True, file_extension='mp4')
+    yt.order_by('resolution')
+    yt.desc()
+    yt.last()
     a=input(f'Download {title}? (Y/N) ')
     if a == 'Y' or a == 'y':
         print('Downloading...')
