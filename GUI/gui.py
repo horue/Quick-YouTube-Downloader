f4909befe06eafd3115b2e09b51d99ec73f7981c
Jorge <jorgejapao00@gmail.com>
2024-04-30 10:34:41 -0300

    Entry for video link added

--- GUI/gui.py
+++ GUI/gui.py
@@ -4,6 +4,9 @@ def main(root):
     l1=ct.CTkLabel(root, text='Quick YouTube Downloader')
     l1.pack(pady=20)
 
+    e1=ct.CTkEntry(root, placeholder_text="Enter the link")
+    e1.pack(pady=20)
+
 def root():
     root = ct.CTk()
     root.geometry("500x350")
