073e8283a098401cff2489be18f3d9f0a2b551cf
Jorge <jorgejapao00@gmail.com>
2024-04-29 22:32:22 -0300

    Main screen structure added

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,1 +1,20 @@
-import customtkinter
+import customtkinter as ct
+
+def main(root):
+    l1=ct.CTkLabel(root, text='Quick YouTube Downloader')
+    l1.pack(pady=20)
+
+def root():
+    root = ct.CTk()
+    root.geometry("500x350")
+    root.title("Quick YouTube Downloader")
+
+    
+    main(root)
+    
+    root.iconify()
+    root.update()
+    root.deiconify()
+    root.mainloop()
+
+root()
