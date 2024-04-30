c69b60e31b23e24be861c889cc7ace6ec406e408
Jorge <jorgejapao00@gmail.com>
2024-04-30 10:37:42 -0300

    Button to show download option added

--- GUI/gui.py
+++ GUI/gui.py
@@ -9,9 +9,12 @@ def main(root):
     e1=ct.CTkEntry(root, placeholder_text="Enter the link")
     e1.pack(pady=20)
 
+    b1=ct.CTkButton(root, text="Show download options")
+    b1.pack()
+
 def root():
     root = ct.CTk()
-    root.geometry("500x350")
+    root.geometry("500x650")
     root.title("Quick YouTube Downloader")
 
     
